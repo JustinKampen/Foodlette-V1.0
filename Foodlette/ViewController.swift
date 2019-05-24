9fb6b6f08a38ac4393b8a7aed5da657390a9cac5
Justin <justin.kampen@gmail.com>
2019-05-24 13:47:27 -0700

    Initial Commit

---
+++ Foodlette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foodlette
+//
+//  Created by Justin Kampen on 5/24/19.
+//  Copyright © 2019 Justin Kampen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
